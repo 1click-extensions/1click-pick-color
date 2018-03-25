f8b2106045c97cef4e9044594b24424745b80c74
Itsick <any.site.n.site@gmail.com>
2018-03-25 10:33:15 +0300

    add urls

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-//chrome.runtime.setUninstallURL("https://1ce.org");
+chrome.runtime.setUninstallURL("https://1ce.org");
 
 if (!localStorage.created) {
-  //chrome.tabs.create({ url: "https://1ce.org" });
+  chrome.tabs.create({ url: "https://1ce.org" });
   var manifest = chrome.runtime.getManifest();
   localStorage.ver = manifest.version;
   localStorage.created = 1;
